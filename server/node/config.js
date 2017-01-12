eb1fff0dfce2d81f16065fff7c3b44fb7022af78
lodexinc <lodexinc@gmail.com>
2017-01-12 02:22:04 -0500

    Signed-off-by: lodexinc <lodexinc@gmail.com>

---
+++ server/node/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    // App Settings
+    MONGO_URI: process.env.MONGO_URI || 'lodexinc.com',
+    TOKEN_SECRET: process.env.TOKEN_SECRET || '9de8a4a2-d9bb-4b55-a05b-7741d112e6d2-c6cfcaa9-9984-4a2b-a1a7-808ca68e069d',  //https://www.guidgen.com/
+
+    // OAuth 2.0
+    FOURSQUARE_SECRET: process.env.FOURSQUARE_SECRET || 'JEOPQEYPEYGT0IVJURISV5OS0LCLO0P30FSKSX40FVBYBD0H',//https://foursquare.com/developers/app/HYTLB0VVK2AG3YLGABOJZLILNQ01XIVR5S4XEX1NTURTN5O1
+    FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || '7f111142fe55e14c28d55a6695f898b8', //APP ID: 355196134850037    https://developers.facebook.com/apps/355196134850037/settings/
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'Q735dPFPB9f54qjPNkxTbABk',  //777278616168-5vo3gd05e6rs0gfm5iss05iv12r12ujs.apps.googleusercontent.com https://console.developers.google.com/apis/credentials?project=satellizer-lodexinc-com
+    GITHUB_SECRET: process.env.GITHUB_SECRET || '75747d72444fd4e2fdcfebac5fbfe31a64ddbc61', // https://github.com/settings/applications/453658
+    LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'zzV57lnpz7TS1kNZ', //  https://www.linkedin.com/developer/apps/4347774/auth
+    WINDOWS_LIVE_SECRET: process.env.WINDOWS_LIVE_SECRET || 'tMkYawxv5q35pTWmPxPE7PM',  //https://apps.dev.microsoft.com/?mkt=en-us#/application/271f5f30-573a-40d8-822a-97b243b207a8
+
+    // OAuth 1.0
+    //LODEXINC (751134800627412992) satellizer.lodexinc.com
+    //https://apps.twitter.com/app/13221567/keys
+    TWITTER_KEY: process.env.TWITTER_KEY || 'oCGryhv6DLuuSmC3HjoUnUCwg',
+    TWITTER_SECRET: process.env.TWITTER_SECRET || 'rDSxv2rov7oVThFWyywpQJ9UJJsg5OnblEQQbjy8KLiZRLmXZW'
+};
+
+
+//  https://apps.twitter.com/app/13221567/keys
+//  https://developers.facebook.com/apps/355196134850037/settings/
+//  https://console.developers.google.com/apis/credentials?project=satellizer-lodexinc-com
+//  https://github.com/settings/applications/453658
+//  https://apps.dev.microsoft.com/?mkt=en-us#/application/271f5f30-573a-40d8-822a-97b243b207a8
+//  https://www.linkedin.com/developer/apps/4347714/auth
